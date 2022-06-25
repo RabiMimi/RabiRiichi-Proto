5957e7bbf8e03965ff71b562acf0e3abccc4b773
KCFindstr <shimakaze@rabimimi.com>
2022-06-24 22:35:22 -0700

    Add copy script

---
+++ copy.sh
@@ -0,0 +1,5 @@
+mkdir -p ../RabiRiichi/Generated/
+mkdir -p ../RabiRiichi-Cocos/assets/Scripts/Generated/
+cp -r ./CS/Generated/* ../RabiRiichi/Generated/
+cp -r ./CS/Server/* ../RabiRiichi/RabiRiichi.Server/
+cp -r ./TS/* ../RabiRiichi-Cocos/assets/Scripts/Generated/
